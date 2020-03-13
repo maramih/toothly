db1ff9fe667d7e95dd75646ba20b94a25c84e80c
Iulia Mihaiu <maramih.2007@yahoo.com>
2020-03-13 15:12:57 +0200

    Added UserData class.

--- toothly/lib/models/user.dart
+++ toothly/lib/models/user.dart
@@ -4,3 +4,15 @@ class User{
 
   User({this.uid});
 }
+
+class UserData{
+
+  final String uid;
+  final String firstname;
+  final String surname;
+  final String role;
+  final int age;
+
+  UserData({this.uid, this.firstname,this.surname,this.role,this.age});
+
+}
