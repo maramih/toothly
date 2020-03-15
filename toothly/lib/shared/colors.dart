b2f22ca384077c0fec28e4cc2035a1410770cad6
Iulia Mihaiu <maramih.2007@yahoo.com>
2020-03-15 07:05:46 +0200

    Add class for color swatches.

---
+++ toothly/lib/shared/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Swatches{
+  static const myPrimaryMint=Color(0xff6ed3cf);
+  static const mySecondaryMint=Color(0xffb8dbda);
+  static const myPrimaryRed=Color(0xffe62739);
+  static const myPrimaryPurple=Color(0xff9068be);
+  static const myPrimaryGrey=Color(0xffe1e8f0);
+}
