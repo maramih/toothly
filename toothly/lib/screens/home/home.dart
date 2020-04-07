5e14c4ba474378f87da679f0d15ff3fec26e7c8f
Iulia Mihaiu <maramih.2007@yahoo.com>
2020-04-08 00:43:21 +0300

    modulation for Home screen & updated packages

--- toothly/lib/screens/home/home.dart
+++ toothly/lib/screens/home/home.dart
@@ -1,56 +1,59 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:toothly/models/profile.dart';
-import 'file:///D:/FlutterApps/Licenta/toothly/lib/screens/home/components/profile_list.dart';
-import 'file:///D:/FlutterApps/Licenta/toothly/lib/screens/home/components/settings.dart';
+import 'package:toothly/shared/constants.dart';
+import '../profile/components/profile_list.dart';
+import '../settings/settings_form.dart';
 import 'package:toothly/services/auth.dart';
 import 'package:provider/provider.dart';
 import 'package:toothly/services/database.dart';
 import 'package:toothly/shared/colors.dart';
 
 class Home extends StatelessWidget {
-
-  final AuthService _auth=AuthService();
-
+  final AuthService _auth = AuthService();
+  final double iconSize= 40;
   @override
   Widget build(BuildContext context) {
-
-    void _showSettingsPanel(){
-      showModalBottomSheet(context: context, builder: (context){
-       return Container(
-         padding: EdgeInsets.symmetric(
-           vertical: 20.0,
-           horizontal: 60.0,
-         ),
-         child: SettingsForm(),
-       );
-      });
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 20.0,
+                horizontal: 60.0,
+              ),
+              child: SettingsForm(),
+            );
+          });
     }
-    
+
+
+
+    final upperBar=AppBar(
+      title: Text('Menu'),
+      backgroundColor: Swatches.myPrimaryMint,
+      elevation: 0.0,
+      actions: <Widget>[
+        FlatButton.icon(
+            onPressed: () => _showSettingsPanel(),
+            icon: Icon(Icons.settings),
+            label: Text('Settings')),
+        FlatButton.icon(
+            onPressed: () async {
+              await _auth.singOut();
+            },
+            icon: Icon(Icons.person),
+            label: Text('LogOut'))
+      ],
+    );
+
     return StreamProvider<List<Profile>>.value(
       value: DatabaseService().profiles,
       child: Scaffold(
         backgroundColor: Swatches.mySecondaryMint,
-        appBar: AppBar(
-          title: Text('Menu'),
-          backgroundColor:Swatches.myPrimaryMint,
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-                onPressed: ()=> _showSettingsPanel(),
-                icon: Icon(Icons.settings),
-                label: Text('Settings')
-            ),
-            FlatButton.icon(
-                onPressed: () async{
-                  await _auth.singOut();
-                },
-                icon: Icon(Icons.person),
-                label: Text('LogOut')
-            )
-          ],
-        ),
+        appBar: upperBar,
         body: ProfileList(),
+        bottomNavigationBar: bottomBar,
       ),
     );
   }
