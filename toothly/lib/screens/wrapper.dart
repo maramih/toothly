0a02eb628c55b742086f1061bf206d9a48808a6a
Iulia Mihaiu <maramih.2007@yahoo.com>
2020-06-07 18:45:12 +0300

    updated dependencies

--- toothly/lib/screens/wrapper.dart
+++ toothly/lib/screens/wrapper.dart
@@ -3,17 +3,27 @@ import 'package:provider/provider.dart';
 import 'package:toothly/models/user.dart';
 import 'package:toothly/screens/authenticate/authenticate.dart';
 import 'package:toothly/screens/home/home.dart';
+import 'package:toothly/screens/profile/my_profile.dart';
+import 'package:toothly/services/database.dart';
+import 'package:toothly/shared/strings.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
-    print(user);
+    print(user.toString());
 
-    if(user==null){
+    if(user==null)
       return Authenticate();
-    } else{
-      return Home();
-    }
+    else
+    return StreamBuilder<UserData>(
+        stream: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot){
+          if(snapshot.hasData && snapshot.data.firstname==NO_DATA)
+            return MyProfile();
+          else
+            return Home();
+        }
+    );
   }
 }
